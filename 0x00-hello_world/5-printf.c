a481cc047df2687738351ba08d6964b267895f9f
Anthony Kaiser <anthonykaisertoniz@gmail.com>
2023-09-07 18:54:53 -0700

    Using the printf function

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - Initiates the programme
+ *
+ * Description: Printing out a command using printf
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
